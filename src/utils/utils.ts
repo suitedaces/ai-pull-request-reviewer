0838273ab918e6a116880fec9df121b295ece322
ishan <ishannagpal554@gmail.com>
2023-12-24 00:01:47 -0600

    debugging

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,7 +3,6 @@ import { File, Chunk, PRMetadata } from "./types";
 export function createPrompt(file: File, chunk: Chunk, prDetails: PRMetadata): string {
   // Instructions for AI Review Process
   const instructions = `Respond in JSON format: {"reviews": [{"lineNumber": <line_number>, "reviewComment": "<review comment>"}]}. 
-                        DO NOT return a lineNumber that's blank in the diff, instead choose the line number that's closest to the code that needs to be reviewed.
                         Provide only constructive feedback in the reviewComment after reviewing the code for security vulnerabilities and adherence to coding best practices for the given language.
                         Make recommendations to make code more secure,faster easier to maintain, reduce redundancy.
                         If no improvements are necessary, keep the "reviews" array empty. 
@@ -16,7 +15,7 @@ export function createPrompt(file: File, chunk: Chunk, prDetails: PRMetadata): s
   const codeDiff = `Code to Review (File: ${file.path}): \`\`\`diff ${chunk.content} ${chunk.changes.map(c => `${c.start_line ? c.start_line : c.end_line} ${c.content}`).join("\n")} \`\`\``;
 
   const prompt = `Review Task: ${instructions} Context: ${prContext} \n Code Diff: ${codeDiff}`;
-
+  console.log("Prompt: ", prompt)
   return prompt;
 }
 
