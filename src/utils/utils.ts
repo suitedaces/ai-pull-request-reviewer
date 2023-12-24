16051bd1a9f71444c6147336db98297d4895a6ee
ishan <ishannagpal554@gmail.com>
2023-12-23 19:17:46 -0600

    prompt update

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,7 +3,7 @@ import { File, Chunk, PRMetadata } from "./types";
 export function createPrompt(file: File, chunk: Chunk, prDetails: PRMetadata): string {
   // Instructions for AI Review Process
   const instructions = `Respond in JSON format: {"reviews": [{"lineNumber": <line_number>, "reviewComment": "<review comment>"}]}. 
-                        Provide only constructive feedback after reviewing the code for security vulnerabilities and adherence to coding best practices for the given language.
+                        Provide only constructive feedback in the reviewComment after reviewing the code for security vulnerabilities and adherence to coding best practices for the given language.
                         Make recommendations to make code more secure,faster easier to maintain, reduce redundancy.
                         If no improvements are necessary, keep the "reviews" array empty. 
                         Use GitHub Markdown format for comments. 
